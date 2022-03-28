9cc41f01e4294c84160d64dff418d18bce818a5a
bmedeirosqa <bmedeiros@twilio.com>
2022-03-28 16:21:40 -0300

    updating

--- src/routes/callbacks/templates.js
+++ src/routes/callbacks/templates.js
@@ -37,7 +37,7 @@ const handleGetTemplatesByCustomerIdCallback = async (req, res) => {
         display_name: 'Openers', // Category name
         templates: [
             { content: compileTemplate(OPENER_NEXT_STEPS, customerDetails) , whatsAppApproved: false}, // Compiled template
-            { content: compileTemplate(OPENER_NEW_PRODUCT, customerDetails) , whatsAppApproved: true},
+            { content: compileTemplate(OPENER_NEW_PRODUCT, customerDetails) , whatsAppApproved: false},
             { content: compileTemplate(OPENER_ON_MY_WAY, customerDetails), whatsAppApproved: true },
             { content: compileTemplate(OPENER_ON_MY_HOUSE, customerDetails), whatsAppApproved: true },
         ]
